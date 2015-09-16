09feab9ed31a462527794c7c8c085c086f8236f0
rodofloni <romanr@hotmail.nl>
2015-09-16 08:52:20 +0100

    Square

---
+++ PythonApplication2/Square.py
@@ -0,0 +1,10 @@
+﻿size = int(input("Enter a number: "))
+startsize = size
+space = ' '
+n = 0
+
+while size != 1:
+    print ('*' * size + space * n + '*')
+    size = size-1
+    n = n+1
+print ('*' * size + '*')
