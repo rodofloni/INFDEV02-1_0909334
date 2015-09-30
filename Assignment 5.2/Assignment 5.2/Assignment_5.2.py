3030f3be8cf781ea6ddeda0d7f0759e808a0ed69
rodofloni <romanr@hotmail.nl>
2015-09-30 10:22:19 +0100

    Assignment 5.2

---
+++ Assignment 5.2/Assignment 5.2/Assignment_5.2.py
@@ -0,0 +1,36 @@
+import re
+while True:
+    password = input("Enter the desired Password: ")
+    if len(password) < 1:
+        print()
+        print("You are taking me in the mailing")
+        print()
+
+
+    pw_score = 0
+
+    if re.search(r'[A-Z]', password):
+        pw_score = pw_score + 1
+    if re.search(r'[a-z]', password):
+       pw_score = pw_score + 1
+    if re.search(r'[0-9]', password):
+        pw_score = pw_score + 1
+    if re.search(r' ', password): 
+        pw_score = pw_score + 10
+
+    if pw_score == 0:
+        print("Password is non-existant")
+    if pw_score <= 2 and pw_score > 0:
+        print("Password is Weak")
+    if pw_score == 3 and len(password) > 6:
+        print("Password is Medium")
+    if pw_score == 3 and len(password) < 6:
+        print("Password is Weak")
+    if pw_score > 3 and len(password) < 6:
+        print("Password is Weak")
+    if pw_score > 3 and len(password) < 18 and len(password) > 6:
+        print("Password is Medium")
+    if pw_score > 3 and len(password) > 18:
+        print("Password is Strong") 
+
+    break 
