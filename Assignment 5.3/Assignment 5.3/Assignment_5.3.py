747167867016772cd43aedaddc3e8e07bcfa1bf3
rodofloni <romanr@hotmail.nl>
2015-09-30 11:19:00 +0100

    Assignment 5.3

---
+++ Assignment 5.3/Assignment 5.3/Assignment_5.3.py
@@ -0,0 +1,28 @@
+import re
+import sys
+while True:
+  txt = input("||Text:   ")
+  shift = int(input("||Steps:  "))
+  length = len(txt)
+  counter = 0
+
+  while counter != length:
+    char = int(ord(txt[counter]))
+    if re.search(r'[a-z]', txt[counter]):
+        newchar = char + shift
+        while newchar >= 123:
+            change = newchar - 122
+            newchar = 96 + change
+    elif re.search(r'[A-Z]', txt[counter]):
+        newchar = char + shift
+        while newchar >= 91:
+            change = newchar - 90
+            newchar = 64 + change
+    else:
+        newchar = char
+    sys.stdout.write(chr(newchar))
+    counter = counter + 1
+  sys.stdout.write("\n")
+  break
+
+#by defining the ascii values 
