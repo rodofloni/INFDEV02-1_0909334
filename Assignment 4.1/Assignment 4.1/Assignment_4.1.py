d9eba3d9489a78ae52ab17d2580bf43e0d5efe49
rodofloni <romanr@hotmail.nl>
2015-10-05 20:06:53 +0100

    Assignment 4.1 Python 2.7 fix

---
+++ Assignment 4.1/Assignment 4.1/Assignment_4.1.py
@@ -0,0 +1,8 @@
+﻿name = raw_input("What is your name?: ")
+
+temp = input("What is the temperature in Fahrenheit?: ")
+digittemp = int(temp)
+
+celc = (digittemp - 32) / 1.8
+celcEnd = round(celc,2)
+print celcEnd
